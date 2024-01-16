090dd346d181b49861e278635f63d6cb35206944
yoann <sayo97278@hotmail.fr>
2024-01-16 10:41:32 +0530

    gi

--- Frontend/components/Home.js
+++ Frontend/components/Home.js
@@ -1,13 +1,9 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-<<<<<<< HEAD
 //const {width, height} = Dimensions.get('window') //detection dela dimension ecran
 
-const Home = ({ navigation }) => {
-=======
-const Home = () => {
->>>>>>> e2f7c889a680dc410aa9b98f0ffc659daee3ced0
+const Home = () => {oa
   return (
     <View>
       <Text>Home</Text>
