b57fc583243a4f08cd945a2bdc2b0f388063b8a5
hkonate <konate.tronca@gmail.com>
2024-01-16 12:17:21 +0100

    AuthToken model

--- Backend/models/User.go
+++ Backend/models/User.go
@@ -1,6 +1,7 @@
 package models
 
 import "gorm.io/gorm"
+
 type User struct {
 	gorm.Model
 	Firstname string
@@ -8,8 +9,7 @@ type User struct {
 	Pseudo    string
 	Email     string
 	Password  string
-	Group []*Group `gorm:"many2many:group_user;"`
-	Bet []Bet
+	Group     []*Group `gorm:"many2many:group_user;"`
+	Bet       []Bet
+	AuthToken []AuthToken
 }
-
-
