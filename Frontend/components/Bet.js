ce89cc24b7e54f2aabfd0bff09361e998423a2c5
Walid Jeghnine <jeghninewalid@outlook.fr>
2024-01-24 17:04:20 +0100

    showing fighters on bet page

--- Frontend/components/Bet.js
+++ Frontend/components/Bet.js
@@ -7,7 +7,7 @@ const Bet = () => {
   const [betOfUserWinner, setBetOfUserWinner] = useState(null);
   const [matchIdOfUser, setMatchId] = useState(null);
   const [matchIdOfEventByDate, setMatchIdOfEventByDate] = useState(null);
-  const [fightersName, setFightersName] = useState(null);
+  const [fightersName, setFightersName] = useState([]);
 
   useEffect(() => {
     const getBetOfUserByGroupId = async () => {
@@ -24,7 +24,7 @@ const Bet = () => {
         const json = await response.json();
         const matchIdOfUser = json.message.map((id) => id.MatchID);
         setMatchId(matchIdOfUser);
-        console.log("match id of bet of user", matchIdOfUser);
+        // console.log("match id of bet of user", matchIdOfUser);
       } catch (error) {
         console.log("Error message", error);
       }
@@ -44,15 +44,17 @@ const Bet = () => {
         const matchIdOfEventByDate = json.summaries.map((sportEvent) => {
           const obj = {};
           obj["sportEventID"] = sportEvent.sport_event.id;
-          obj["sportEventCombatant1"] = sportEvent.sport_event.competitors[0].name;
-          obj["sportEventCombatant2"] = sportEvent.sport_event.competitors[1].name;
+          obj["sportEventCombatant1"] =
+            sportEvent.sport_event.competitors[0].name;
+          obj["sportEventCombatant2"] =
+            sportEvent.sport_event.competitors[1].name;
           return obj;
         });
 
-        setMatchIdOfEventByDate(matchIdOfEventByDate);
-        console.log("mes matchs id", matchIdOfEventByDate);
+        setMatchIdOfEventByDate([matchIdOfEventByDate]);
+        // console.log("mes matchs id", matchIdOfEventByDate.map());
       } catch (error) {
-        console.log("Error message", error);
+        console.log("Error messagee", error);
       }
     };
     getBetOfUserByGroupId();
@@ -60,28 +62,52 @@ const Bet = () => {
   }, []);
   useEffect(() => {
     if (matchIdOfUser && matchIdOfEventByDate) {
-      matchIdOfUser.forEach((userMatchId) => {
-        if (matchIdOfEventByDate.includes(userMatchId)) {
-          console.log(`Correspondance Trouvé ${userMatchId}`);
+      combatDesParisDuUser = [];
+      matchIdOfEventByDate[0].map((matchOfEvent, i) => {
+        if (matchIdOfUser.includes(matchOfEvent["sportEventID"])) {
+          setFightersName((prev) => [...prev, matchOfEvent]);
+          // console.log(fightersName[0])
         }
       });
-    }
 
+      // console.log(fightersName.forEach((element) => {
+      //   console.log("combatant 1",element,"combatant2",element)
+
+      // }))
+      // console.log(combatDesParisDuUser.forEach((combat)=>combat[sportEventCombatant1]))
+      // matchIdOfEventByDate["sportEventID"].map((matchOfEvent, i) => {
+      //   console.log(`nombre de repetion${i}`,matchOfEvent);
+      //   if (matchOfEvent.includes(matchIdOfUser)) {
+      //     console.log(`Correspondance Trouvé  ${matchIdOfEventByDate}`);
+      //   }
+      // });
+    }
     // if (matchIdOfUser !== null) {
     //   console.log("match id of userr", matchIdOfUser);
     // }
-  }, [matchIdOfUser, matchIdOfEventByDate]);
+  }, []);
 
   const handeTextClick = () => {};
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={handeTextClick}>
-        <Text>{matchIdOfUser}</Text>
-      </TouchableOpacity>
+      {fightersName?.map((el) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate("MyTopTabs", group)}
+          key={el.sportEventID}
+        >
+          <Text>
+            {el.sportEventCombatant1} vs {el.sportEventCombatant2}
+          </Text>
+        </TouchableOpacity>
+      ))}
     </View>
+    // <View style={styles.container}>
+    // {  fightersName?.map((el)=>{
+    //     <Text>{`${el.sportEventCombatant1} vs ${el.sportEventCombatant2}`}</Text>
+    // })}
+    // </View>
   );
 };
-
 export default Bet;
 
 const styles = StyleSheet.create({
