69bae4f0e8fc5f12208a0f49b06a1f0b52b68fa0
Lachignol <110435478+Lachignol@users.noreply.github.com>
2024-06-11 16:44:51 +0200

    working on PointPerBetfunction

--- Backend/Scheduler/Scheduler.go
+++ Backend/Scheduler/Scheduler.go
@@ -44,7 +44,7 @@ type Response struct {
 
 func Match() (string, error) {
 
-	resp, err := http.Get("https://api.sportradar.com/mma/trial/v2/fr/schedules/2024-02-04/summaries.json?api_key=nrmu6fxvt5e5bzdzhx2845fq")
+	resp, err := http.Get("https://api.sportradar.com/mma/trial/v2/fr/schedules/2024-02-04/summaries.json?api_key=NJLPJJ0QzW9CSb26DmAE9a0j54ce8Kkq46d84rDl")
 	if err != nil {
 		log.Fatalln(err)
 		return "", err
@@ -68,8 +68,7 @@ func Match() (string, error) {
 	for _, summary := range responseData.Summaries {
 		// fmt.Printf("Statut de l'événement sportif : %s\n", summary.SportEvent.SportEventId)
 		if summary.SportEventStatus.Status == "cancelled" {
-			var matchcancelled string
-			matchcancelled = "Match cancelled"
+			matchcancelled := "Match cancelled"
 			resultofbet := models.ResultOfBet{
 				MatchCancelled: &matchcancelled,
 			}
@@ -132,15 +131,106 @@ func PointPerBet() {
 
 	var ResultOfBet []models.ResultOfBet
 
-	database.DB.Preload("ResultOfBet").Where("result_of_bet_id IS NOT NULL").Find(&Bets,&ResultOfBet)
+	database.DB.Preload("ResultOfBet").Where("result_of_bet_id IS NOT NULL").Find(&Bets, &ResultOfBet)
 	// database.DB.Where("ResultOfBet = ?","!= nil").Find(&Bets)
-	for i:=0; i<len(Bets); i++ {               // start of the execution block
-        fmt.Println(Bets[i].ID)  
-		fmt.Println(*Bets[i].ResultOfBet.Winner)
-		// fmt.Println(*Bets[i].ResultOfBet)
-		fmt.Println(Bets[i].ResultOfBet.MatchID)   // prints "Hello" 3 times
-		fmt.Println(Bets[i].Winner)   
-    }    
-	
+	for i := 0; i < len(Bets); i++ { // start of the execution block
+		points := 0
+		if Bets[i].ResultOfBet.MatchCancelled != nil {
+			fmt.Println("Match annulé!!!")
+			fmt.Println(*Bets[i].ResultOfBet.MatchCancelled)
+		} else {
 
+			fmt.Println("-----------------------------------")
+			fmt.Println(Bets[i].ResultOfBet.MatchCancelled)
+			fmt.Println("Match id du bet:")
+			fmt.Println(Bets[i].ID)
+			fmt.Println("Match id du resultOfBet: ")
+			fmt.Println(Bets[i].ResultOfBet.MatchID)
+			fmt.Println("-------")
+			fmt.Println("winner du bet: ")
+			if Bets[i].Winner != nil {
+				if *Bets[i].Winner == *Bets[i].ResultOfBet.Winner {
+					points += 1
+					fmt.Println(*Bets[i].Winner)
+					fmt.Println("Bon vainqueur parié")
+				} else {
+					fmt.Println(*Bets[i].Winner)
+					fmt.Println("mauvais vainqueur parié")
+				}
+			} else {
+				fmt.Println("pas de winner parié")
+			}
+			fmt.Println("winner du resulat: ")
+			fmt.Println(*Bets[i].ResultOfBet.Winner)
+			fmt.Println("-------")
+			fmt.Println("Finish du bet:")
+			if Bets[i].FinishMethod != nil {
+				if *Bets[i].FinishMethod == *Bets[i].ResultOfBet.FinishMethod && *Bets[i].Winner == *Bets[i].ResultOfBet.Winner {
+					fmt.Println(*Bets[i].FinishMethod)
+					points += 2
+				} else {
+					fmt.Println(*Bets[i].FinishMethod)
+					fmt.Println("Mauvais finish parié")
+				}
+			} else {
+				fmt.Println("pas de finish parié")
+			}
+
+			fmt.Println("Finish du resultat:")
+			fmt.Println(*Bets[i].ResultOfBet.FinishMethod)
+			fmt.Println("-------")
+			fmt.Println("Rounds parié du bet :")
+			if Bets[i].Rounds != nil {
+				fmt.Println(*Bets[i].Rounds)
+				// if *Bets[i].Rounds[0] == *Bets[i].ResultOfBet.Rounds[0]{
+				// points+= 10
+				// fmt.Println("Bon round 1 parié")
+				// }else{
+				// 	fmt.Println("Mauvais round parié pas round 1 ")
+				// }
+				// if *Bets[i].Rounds[1] == *Bets[i].ResultOfBet.Rounds[1]{
+				// points+= 10
+				// fmt.Println("Bon round 2 parié")
+				// }else{
+				// 	fmt.Println("Mauvais round parié pas round 2 ")
+				// }
+				// if *Bets[i].Rounds[2] == *Bets[i].ResultOfBet.Rounds[2]{
+				// points+= 10
+				// fmt.Println("Bon round 3 parié")
+
+				// }else{
+				// 	fmt.Println("Mauvais round parié pas round 3 ")
+				// }
+				// if *Bets[i].Rounds[3] == *Bets[i].ResultOfBet.Rounds[3]{
+				// 	points+= 10
+				// 	fmt.Println("Bon round 4 parié")
+				// 	}else{
+				// 		fmt.Println("Mauvais round parié pas round 4 ")
+				// 	}
+				// if *Bets[i].Rounds[4] == *Bets[i].ResultOfBet.Rounds[4]{
+				// 		points+= 10
+				// 	    fmt.Println("Bon round 5 parié")
+
+				// 		}else{
+				// 		fmt.Println("Mauvais round parié pas round 5 ")
+				// 		}
+				// if *Bets[i].Rounds == *Bets[i].ResultOfBet.Rounds {
+
+			} else {
+				fmt.Println("pas de round parié")
+			}
+			fmt.Println("Rounds parié du resultat : ")
+			fmt.Println(*Bets[i].ResultOfBet.Rounds)
+			fmt.Println("-------")
+			fmt.Println("total des points")
+			fmt.Println(points)
+			fmt.Println("Match status ( rien si pas annulé): ")
+			fmt.Println(Bets[i].ResultOfBet.MatchCancelled)
+			fmt.Println("-----------------------------------")
+
+			// prints "Hello" 3 times
+
+		}
+
+	}
 }
