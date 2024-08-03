5a84e693ef2d24a148c822e27eb4ec76fa2b6834
Lachignol <110435478+Lachignol@users.noreply.github.com>
2024-08-03 16:55:10 +0200

    accountupdate in process

--- Frontend/components/Account.js
+++ Frontend/components/Account.js
@@ -1,10 +1,71 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity,SafeAreaView,TextInput,Button} from 'react-native'
+import React, { useState } from 'react'
 import * as SecureStore from 'expo-secure-store';
+
 //const {width, height} = Dimensions.get('window') //detection dela dimension ecran
 
-const Account = ({setUser}) => {
+const Account = ({setUser,user}) => {
+  const User = user.user
+  const [state, setState] = useState({
+    Pseudo: User.Pseudo,
+    Firstname: User.Firstname
+  });
+
+  const handleInputChange = (name ,value) => {
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+
+  const handleSubmit=(e)=>{
+    try {
+      e.preventDefault()
+      console.log("insubmit")
+      const { Pseudo, Firstname } = state;
+      console.log("pseudo",Pseudo)
+      console.log("firstname",Firstname)
+      // let formdata = new FormData();
+
+      // formdata.append("product[name]", 'test')
+      // formdata.append("product[price]", 10)
+      // formdata.append("product[category_ids][]", 2)
+      // formdata.append("product[description]", '12dsadadsa')
+      // formdata.append("product[images_attributes[0][file]]", {uri: photo.uri, name: 'image.jpg', type: 'image/jpeg'})
+      // fetch('url',{
+      //   method: 'post',
+      //   headers: {
+      //     'Content-Type': 'multipart/form-data',
+      //   },
+      //   body: formdata
+      //   }).then(response => {
+      //     console.log("image uploaded")
+      //   }).catch(err => {
+      //     console.log(err)
+      //   })  
+
+    
+  
+
+
+
+    } catch (error) {
+      
+    }
+
+
+
+  }
+
+ 
+
+
+  
+  
+
   return (
+    <>
     <View style={styles.container}>
       <Text>Account</Text>
       <TouchableOpacity style={styles.customButton} onPress={async()=>{
@@ -12,6 +73,34 @@ const Account = ({setUser}) => {
         setUser(null)
       }}><Text style={styles.buttonText}>Se déconnecter</Text></TouchableOpacity>
     </View>
+
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        name="Pseudo"
+        value={state.Pseudo}
+        onChangeText={(txt)=>handleInputChange("Pseudo",txt)}
+      />
+      <TextInput
+        style={styles.input}
+        name="Firstname"
+        value={state.Firstname}
+        onChangeText={(txt)=>handleInputChange("Firstname",txt)}
+        keyboardType="numeric"
+      />
+       <Button title="Submit" onPress={handleSubmit} />
+    </SafeAreaView>
+ 
+      {/* <View style={styles.form}>
+        <Form>
+          <Input name="name" label="Name" value={User.Pseudo} />
+          <Input name="email" label="Email" />
+          <Input name="password" label="Password" secureTextEntry={true} />
+          <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+        </Form>
+      </View> */}
+
+    </>
   )
 }
 
@@ -36,6 +125,12 @@ const styles = StyleSheet.create({
     
   },
   buttonText: {
+    color: "white",
+    fontWeight: "700",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  form: {
     color: "white",
     fontWeight: "700",
     alignItems: "center",
