c8393fd1fdd822bcd9903c4fb390c06599e80bab
Lachignol <110435478+Lachignol@users.noreply.github.com>
2024-08-05 00:34:25 +0200

    ajout de l'upload de l'avatar dans le back (modifier nom du fichier uploader et son emplacement

---
+++ Backend/helper/upload.go
@@ -0,0 +1,18 @@
+package helper
+
+import (
+	"log"
+	"mime/multipart"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UploadFile(c *gin.Context, file *multipart.FileHeader) (string, error) {
+	dst := "./views/assets/image/" + file.Filename
+	err := c.SaveUploadedFile(file, dst)
+	if err != nil {
+		return "", err
+	}
+	log.Printf("'%s' uploaded!", file.Filename)
+	return file.Filename, nil
+}
--- Backend/models/User.go
+++ Backend/models/User.go
@@ -9,6 +9,7 @@ type User struct {
 	Pseudo    string
 	Email     string
 	Password  string
+	PathOfAvatar  string
 	Groups    []*Group `gorm:"many2many:group_users;" json:"groups"`
 	Bets      []*Bet `gorm:"foreignKey:UserID"`
 	AuthToken []AuthToken
