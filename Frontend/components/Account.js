0b2732e91d69ef07a7db998db34b34cd4b2ebe0b
Lachignol <110435478+Lachignol@users.noreply.github.com>
2024-08-05 04:13:11 +0200

    change avatar perfectionement avec erreur et reset etc

--- Frontend/components/Account.js
+++ Frontend/components/Account.js
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, TextInput, Button, Image } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, TextInput, Button, Image,Alert} from 'react-native'
+import React, { useState, useCallback,useEffect } from 'react'
 import { IP } from '@env';
 import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
@@ -8,18 +8,22 @@ const Account = ({setUser, user}) => {
   const [image, setImage] = useState(null);
   const [imageName, setImageName] = useState(null);
   const [imageType, setImageType] = useState(null);
+  const [erreur, setErreur] = useState(null);
+  const [hasPermission, setHasPermission] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const User = user.user
 
-  
-    const requestPermission = async () => {
-      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-      if (status !== 'granted') {
-        alert('Désolé, nous avons besoin des permissions pour accéder à vos photos!');
-      }
-    };
-   ;
+  useEffect(() => {
+    requestPermission();
+  }, [requestPermission]);
  
+  const requestPermission = useCallback(async () => {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    setHasPermission(status === 'granted');
+    console.log(status)
+    return status === 'granted';
+  }, []);
+
 
   const [state, setState] = useState({
     Pseudo: User.Pseudo,
@@ -34,8 +38,20 @@ const Account = ({setUser, user}) => {
   };
 
   const pickImage = async () => {
-    console.log("Début de pickImage");
-    let permission = await requestPermission()
+      
+    let permissionResult = hasPermission;
+    if (permissionResult === null) {
+      permissionResult = await requestPermission();
+    }
+
+    if (!permissionResult) {
+      Alert.alert(
+        "Permission refusée",
+        "Nous avons besoin de votre permission pour accéder à vos photos.",
+        [{ text: "OK" }]
+      );
+      return;
+    }
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
@@ -43,27 +59,32 @@ const Account = ({setUser, user}) => {
       quality: 1,
     });
 
-    console.log("Résultat de la sélection d'image:", result);
-
     if (!result.canceled) {
       setImage(result.assets[0].uri);
       setImageName(result.assets[0].fileName);
       setImageType(result.assets[0].type)
       console.log("Image sélectionnée:", result.assets[0].uri);
       console.log("Nom de l'Image :", result.assets[0].fileName);
-      console.log("Type de l'Image:", result.assets[0].fileName);
+      console.log("Type de l'Image:", result.assets[0].fileName)
+      console.log("",result.assets[0].exif);
     } else {
-      console.log("Sélection d'image annulée");
+      resetImagePicker()
     }
   };
 
+  const resetImagePicker = useCallback(() => {
+    setImage(null);
+    setImageName(null);
+    setImageType(null);
+  }, []);
+
   const uploadImage = async () => {
-    console.log("Début de uploadImage");
+    setErreur(null)
+    console.log("Début");
     if (!image) {
       console.log("Pas d'image sélectionnée");
       return;
     }
-
     setIsLoading(true);
     const formData = new FormData();
     formData.append('Avatar', {
@@ -81,16 +102,16 @@ const Account = ({setUser, user}) => {
           'Content-Type': 'multipart/form-data',
         },
       });
-
       console.log('Statut de la réponse:', response.status);
-
       if (!response.ok) {
         const errorText = await response.text();
+        if(response.status == 424 ){
+          setErreur(`Pas d'image trouvé avez-vous autorisez celle-ci ?`)
+        }
         throw new Error(`HTTP error! status: ${response.status}, message: ${errorText}`);
       }
-
       const result = await response.json();
-      console.log('Succès:', result);
+      Alert.alert("Succès", "L'image a été uploadée avec succès.");
       
       setUser(prevUser => ({
         ...prevUser,
@@ -98,7 +119,8 @@ const Account = ({setUser, user}) => {
           ...prevUser.user,
           PathOfAvatar: imageName
         }
-      }));
+      }))
+      resetImagePicker();
     } catch (error) {
       console.error('Erreur:', error.message);
     } finally {
@@ -132,6 +154,9 @@ const Account = ({setUser, user}) => {
       <View style={styles.imageContainer}>
         <Button title="Choisir une image" onPress={pickImage} />
         {image && <Image source={{ uri: image }} style={styles.image} />}
+        {erreur && <Text style={styles.errorText}>{erreur}</Text>}
+
+    
         <Button 
           title={isLoading ? "Chargement..." : "Envoyer l'image"}
           onPress={uploadImage}
